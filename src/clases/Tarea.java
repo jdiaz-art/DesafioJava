9dc477123a6ee9a53d5f4b6ef743b3ca4d9d1d5e
jdiaz-art <jdiaz@technologylatam.com>
2025-11-14 13:08:43 -0300

    actualizacion

--- src/clases/Tarea.java
+++ src/clases/Tarea.java
@@ -3,24 +3,20 @@ package clases;
 import utilidades.anotaciones.nivelDeImportancia;
 
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.time.temporal.ChronoUnit;
 
-@Importancia(nivel = nivelDeImportancia.Media)
+@Importancia(nivel = nivelDeImportancia.Alta)
 public class Tarea {
 
     private int id;
     private String descripcion;
     private LocalDate fechaVencimiento;
     private boolean completado;
-    private Importancia grado;
 
     public Tarea(int id, String descripcion, LocalDate fechaVencimiento, boolean completado) {
         this.id = id;
         this.descripcion = descripcion;
         this.fechaVencimiento = fechaVencimiento;
         this.completado = completado;
-//        this.grado = nivelImportancia;
     }
 
     public int getId() {
@@ -54,18 +50,6 @@ public class Tarea {
     public void setCompletado(boolean completado) {
         this.completado = completado;
     }
-
-//    private nivelDeImportancia nivelImportancia(){
-//        long estado = ChronoUnit.DAYS.between(getFechaVencimiento(),LocalDate.now());
-//
-//        if(estado <= 5){
-//            return nivelDeImportancia.Alta;
-//        }if(estado >= 10){
-//            return nivelDeImportancia.Baja;
-//        }else {
-//            return nivelDeImportancia.Media;
-//        }
-//    }
 
     @Override
     public String toString() {
